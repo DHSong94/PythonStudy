5c27d8b77a3c79b960587047e709660f426e47ad
DHSong94 <ahsran@gmail.com>
2017-10-08 23:47:15 +0900

    ComputerNetwork HW1

---
+++ ComputerNetworkHW1.py
@@ -0,0 +1,25 @@
+import math
+
+N=0
+n=11
+P=0
+
+def MaxMember(n, N):
+    while 1 :
+
+        result = math.factorial(N)/(math.factorial(n)*math.factorial(N-n)) # NCn 값
+        one = result * (0.1**n) * (0.9**(N-n)) # 한번 계산한 값
+
+        P = P + one
+        print('n이 %d일 때, one은 %0.10e, P는 %0.10e입니다.' % (n, one, P))
+        n = n + 1
+
+        if(n>N):
+            print("연산을 종료합니다.")
+            break
+
+        if(P >= 0.01):
+            print("P=%0.10lf Happy Happy" % P)
+            break
+    return P
+
