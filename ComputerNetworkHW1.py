b650443e87ed42b037a61d678f99579ea83d30ac
DHSong94 <ahsran@gmail.com>
2017-10-09 19:13:49 +0900

    ComputerNetwork HW1 Final

--- ComputerNetworkHW1.py
+++ ComputerNetworkHW1.py
@@ -1,25 +1,32 @@
-# 아직까지는 N값을 수동으로 바꿔야 함
-# 함수로 만들어서 매개변수 집어넣게 만들면 편할 것 같은데 ㅠㅠ
+# MaxMember - n값 변경하면서
 
 import math
 
-N=52
-n=11
-P=0
+def sigma(N) :
+    n=11 # n은 11부터
+    P=0
+    while 1 :
+        result = math.factorial(N)/(math.factorial(n)*math.factorial(N-n)) # NCn 값
+        one = result * (0.1**n) * (0.9**(N-n)) # 한번 계산한 값
 
-while 1 :
+        P+=one # 계산한 값들의 합 (시그마)
+        n+=1 # n값 증가
 
-    result = math.factorial(N)/(math.factorial(n)*math.factorial(N-n)) # NCn 값
-    one = result * (0.1**n) * (0.9**(N-n)) # 한번 계산한 값
+        if(n>N):
+            print("N이 %d일 때 P값은 %0.12lf입니다. %d번째 연산을 종료합니다." % (N, P, N-10))
+            return P
 
-    P = P + one
-    print('n이 %d일 때, one은 %0.10e, P는 %0.10e입니다.' % (n, one, P))
-    n = n + 1
+def MaxMember():  # 최대 가입자 수(M) 구하기
+    M=0
+    N=11 # N은 11부터 시작
 
-    if(n>N):
-        print("연산을 종료합니다.")
-        break
+    while M < 0.01 :
+        M=sigma(N)
 
-    if(P >= 0.01):
-        print("P=%0.10lf Happy Happy" % P)
-        break
+        if(M < 0.01): # M값이 0.01보다 작으면 N값 증가시켜서 MaxMember(N) 다시 실행
+            N+=1
+        else:
+            break
+
+MaxMember()
+#print("N이 %d일 때 M값이 %0.10lf이므로 최대 가입자수는 %d명입니다." %(N, M, N-1))
